896979db91a6b3d71bebe3f44c8426d0baba5aae
porori1224 <iminseo031224@gmail.com>
2024-11-01 17:39:45 +0900

    Feat: Initialize Django project structure for deepfake detection
    - Create 'deepfake_detection' app for handling deepfake detection
    - Set up basic URLs and views for main page and file upload functionality
    
    - Add basic HTML templates (home, upload) for UI structure
    - Configure initial settings and register app in settings.py
    - Implement file upload handler in views to prepare for model integration

--- mysite/deepfake_detection/views.py
+++ mysite/deepfake_detection/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'deepfake_detection/home.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        # 파일 업로드 시 처리(추후 모델 처리 코드 추가 예정)
+        upload_file = request.FILES['file']
+        # 모델 완성 시 파일을 처리하는 로직 추가 예정
+        return HttpResponse("파일이 업로드됨.")
+    return render(request, 'deepfake_detection/upload.html')
