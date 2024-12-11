537be2ca6aa446dfdbb5a5b85b1ab258a7453703
HJ Y <hj021313@gmail.com>
2024-12-12 03:41:16 +0900

    home.html : 비디오 업로드 시 입력 박스 동적 높이 조정 기능 추가, style.css : 동적 높이 조정을 위한 flex-box 설정, views, urls.py 위 css와 코드 맞춰 연동

--- mysite/deepfake_detection/urls.py
+++ mysite/deepfake_detection/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('upload/', views.upload_file, name = 'upload_video'),
+    path('', views.home, name='home'),
+    path('upload/', views.upload_file, name='upload_video'),
 ]
